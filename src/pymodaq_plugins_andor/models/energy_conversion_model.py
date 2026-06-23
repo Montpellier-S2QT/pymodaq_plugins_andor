9ee8afb953f2e90980085e217457c8e13a958e96
Pierre Valvin <pierre.valvin@umontpellier.fr>
2026-06-23 16:35:13 +0200

    working datamixer model

--- src/pymodaq_plugins_andor/models/energy_conversion_model.py
+++ src/pymodaq_plugins_andor/models/energy_conversion_model.py
@@ -8,11 +8,12 @@ class DataMixerEnergyConversionModel(DataMixerModel):
 
     def process_dte(self, dte: DataToExport):
 
-        x_axis = Axis(label='Energy (eV)', data=1239.8/dte[0].axes[0].data)
+        x_axis = Axis(label='Energy (eV)', data=1239.8/dte.data[0].axes[0].data)
 
         dte_processed = DataToExport('computed')
         dte_processed.append(DataCalculated(name='EnergySpectrum',
-                                            data=[np.transpose(dte[0].data)],
+                                            data=dte.data[0].data,
+                                            dim=dte.data[0].dim,
                                             label='Intensity',
                                             axes=[x_axis]))
         return dte_processed
